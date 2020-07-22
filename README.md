2b06b04671b25253e8d3d24710755c606dccf72e
wristypresh360 <65338562+wristypresh360@users.noreply.github.com>
2020-07-22 10:22:35 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# event service Website
+Complete event service Website with Bootstrap - HTML/CSS/JS In this project, we are going to learn and build how to create a complete Event service website with bootstrap using HTML and CSS. We will understand everything from scratch. 
+
+## To execute this project just open index.html file in the browser.
---
+++ form.php
@@ -0,0 +1,35 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "precious.okere777@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
